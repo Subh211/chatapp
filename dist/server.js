b42e2f53d90a3120001e3c24d96baf5c60bcaef4
Subh211 <145745637+Subh211@users.noreply.github.com>
2025-01-07 22:10:51 +0530

    done

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,6 @@
 "use strict";
 // import dotenv from 'dotenv';
+// dotenv.config();
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -13,7 +14,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// dotenv.config();
 // import express from 'express';
 // import http from 'http';
 // import { Server } from 'socket.io';
@@ -26,7 +26,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // const server = http.createServer(app);
 // const io = new Server(server, {
 //     cors: {
-//         origin: '*',  
+//         origin: '*',
 //         methods: ['GET', 'POST'],
 //         allowedHeaders: ['Content-Type'],
 //         credentials: true
@@ -37,12 +37,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // app.use('/api/auth', authRoutes);
 // app.use('/api/chat', chatRoutes);
 // const PORT = process.env.PORT || 5000;
-// const users: { [username: string]: string } = {}; // Use username as key now
+// const users: { [username: string]: string } = {}; 
 // io.on('connection', (socket) => {
 //     console.log('A user connected');
 //     socket.on('join', function (username) {
 //         username = username.trim();
-//         users[username] = socket.id; // Store the username as key and socket.id as value
+//         users[username] = socket.id; 
 //         console.log(`${username} joined the chat`);
 //         console.log(users);
 //     });
@@ -53,8 +53,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //         console.log(`message: ${message}`);
 //         console.log(`receiver: ${receiver}`);
 //         console.log(`sender: ${sender}`);
-//         console.log(users);
-//         const targetSocketId = users[receiver]; // Access receiver directly by username now
+//         const targetSocketId = users[receiver];
 //         if (targetSocketId) {
 //             io.to(targetSocketId).emit('receiveMessage', { sender, message });
 //             console.log(`Message delivered to ${receiver}`);
@@ -62,6 +61,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //             console.log(`User ${receiver} not connected`);
 //         }
 //         await client.rPush('messages', JSON.stringify({ sender, receiver, message, timestamp: new Date() }));
+//     });
+//     socket.on('receiveMessage', (sender, message) => {
+//         console.log(`Message received by ${sender}:`, message);
 //     });
 //     socket.on('disconnect', () => {
 //         const disconnectedUser = Object.keys(users).find(key => users[key] === socket.id);
@@ -83,6 +85,7 @@ const db_1 = __importDefault(require("./config/db"));
 const auth_1 = __importDefault(require("./routes/auth"));
 const chat_1 = __importDefault(require("./routes/chat"));
 const redisClient_1 = __importDefault(require("./services/redisClient"));
+const message_1 = __importDefault(require("./models/message"));
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
 const io = new socket_io_1.Server(server, {
@@ -106,10 +109,6 @@ io.on('connection', (socket) => {
         users[username] = socket.id;
         console.log(`${username} joined the chat`);
         console.log(users);
-        // Automatically register the receiveMessage listener for this user
-        // socket.on('receiveMessage', (data) => {
-        //     console.log(`Message received by ${username}:`, data);
-        // });
     });
     socket.on('message', function (message, receiver, sender) {
         return __awaiter(this, void 0, void 0, function* () {
@@ -127,11 +126,11 @@ io.on('connection', (socket) => {
             else {
                 console.log(`User ${receiver} not connected`);
             }
+            // Save message to Redis
             yield redisClient_1.default.rPush('messages', JSON.stringify({ sender, receiver, message, timestamp: new Date() }));
+            const redisMessageCount = yield redisClient_1.default.lLen('messages');
+            console.log(`Total messages in Redis: ${redisMessageCount}`);
         });
-    });
-    socket.on('receiveMessage', (sender, message) => {
-        console.log(`Message received by ${sender}:`, message);
     });
     socket.on('disconnect', () => {
         const disconnectedUser = Object.keys(users).find(key => users[key] === socket.id);
@@ -141,6 +140,59 @@ io.on('connection', (socket) => {
         }
     });
 });
+// Move messages from Redis to MongoDB every 10 seconds
+// setInterval(async () => {
+//     const messageCount = await client.lLen('messages');
+//     if (messageCount > 0) {
+//         const messageData = await client.lPop('messages'); // Fetch one message
+//         if (messageData) {
+//             const message = JSON.parse(messageData);
+//             const savedMessage = new Message(message);
+//             await savedMessage.save();
+//             console.log('Message saved to MongoDB:', message);
+//         }
+//     }
+// }, 10000); // 10 seconds
+// setInterval(async () => {
+//     const messageCount = await client.lLen('messages');
+//     if (messageCount > 0) {
+//         const messageData = await client.lPopCount('messages',1); // Fetch one message
+//         if (messageData) {
+//             const message = JSON.parse(messageData[0]);
+//             const savedMessage = new Message(message);
+//             await savedMessage.save();
+//             console.log('Message saved to MongoDB:', message);
+//         }
+//     }
+// }, 30000); // 10 seconds
+setInterval(() => __awaiter(void 0, void 0, void 0, function* () {
+    const messageCount = yield redisClient_1.default.lLen('messages');
+    if (messageCount > 0) {
+        const messageData = yield redisClient_1.default.lPopCount('messages', 1); // Fetch one message as an array
+        if (messageData && messageData.length > 0) {
+            const message = JSON.parse(messageData[0]);
+            const participants = [message.sender, message.receiver].sort(); // Ensure consistent ordering
+            // Check if a document already exists for these participants
+            let chat = yield message_1.default.findOne({ participants });
+            if (!chat) {
+                // If no document exists, create a new one
+                chat = new message_1.default({
+                    participants,
+                    messages: [{ sender: message.sender, message: message.message }]
+                });
+            }
+            else {
+                // If document exists, push the new message
+                chat.messages.push({
+                    sender: message.sender,
+                    message: message.message
+                });
+            }
+            yield chat.save();
+            console.log('Message saved to MongoDB:', message);
+        }
+    }
+}), 20000); // 10 seconds
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
--- src/models/message.ts
+++ src/models/message.ts
@@ -1,10 +1,28 @@
+// import { Schema, model } from 'mongoose';
+
+// const messageSchema = new Schema({
+//     sender: String,
+//     receiver: String,
+//     message: String,
+//     timestamp: { type: Date, default: Date.now }
+// });
+
+// const Message = model('Message', messageSchema);
+
+// export default Message;
+
+
 import { Schema, model } from 'mongoose';
 
 const messageSchema = new Schema({
-    sender: String,
-    receiver: String,
-    message: String,
-    timestamp: { type: Date, default: Date.now }
+    participants: [String], // Store both sender and receiver in an array
+    messages: [
+        {
+            sender: String,
+            message: String,
+            timestamp: { type: Date, default: Date.now }
+        }
+    ]
 });
 
 const Message = model('Message', messageSchema);
